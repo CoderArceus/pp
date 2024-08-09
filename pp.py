0bf9df759da482d4f7d32e68520adb86e284624a
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2024-08-10 01:06:24 +0530

    Add files via upload

---
+++ pp.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import os
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages=True
+intents.members = True
+
+bot=commands.Bot(command_prefix="pp ", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+with open("token.txt") as file:
+    token=file.read()
+
+#loading modules and starting bot
+
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+async def main():
+    async with bot:
+        await load()
+        await bot.start(token)
+asyncio.run(main())
