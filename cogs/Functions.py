153a8dbb4c83aa602ccb6005f017c94e44b18a51
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2024-08-11 23:51:25 +0530

    Delete cogs/Functions.py

--- cogs/Functions.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-# Random Color Generator (0-255)
-async def randomColor():
-    r = randint(0, 255)
-    g = randint(0, 255)
-    b = randint(0, 255)
-    randColor = (r, g, b)
