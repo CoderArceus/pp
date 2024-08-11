4a4f7394435c1a17862a991cbaa78b69a5629810
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2024-08-11 22:58:43 +0530

    Add files via upload

--- cogs/userProfileCommands.py
+++ cogs/userProfileCommands.py
@@ -17,14 +17,16 @@ class userProfileCommands(commands.Cog):
             print('2')
             embedAvatarSelf = discord.Embed(title=f'{ctx.message.author}')
             print('3')
-            userAvatar=ctx.message.author.avatar_url
+            userAvatar=member.avatar_url
             print('4')
             embedAvatarSelf.set_image(userAvatar)
             print('5')
             await ctx.reply(embed=embedAvatarSelf)
         else:
             embedAvatarOther = discord.Embed(title=f'{member}')
-            embedAvatarOther.set_image(member.avatar_url)
+            userAvatar=member.avatar_url
+            print('6')
+            embedAvatarOther.set_image(userAvatar)
             await ctx.reply(embed=embedAvatarOther)
 		
 async def setup(bot):
