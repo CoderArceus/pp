785bf082f555834c66ca2edd4e4f5ab55e51f7fd
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2024-08-12 01:39:40 +0530

    Add files via upload

--- utility.py
+++ utility.py
@@ -4,6 +4,5 @@ from discord.ext import commands
 
 # Random Color Generator (0-255)
 def randomColor():
-    randColor = "%06x" % random.randint(0, 0xFFFFFF)
-    print(randColor)
+    randColor = '#{:06x}'.format(random.randint(0, 0xFFFFFF))
     return randColor
