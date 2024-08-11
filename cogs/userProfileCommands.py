680800c0e7099c607c5bb67831da7d43a7d1f168
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2024-08-11 22:22:57 +0530

    Add files via upload

--- cogs/userProfileCommands.py
+++ cogs/userProfileCommands.py
@@ -10,16 +10,16 @@ class userProfileCommands(commands.Cog):
     async def on_ready(self):
         print(f'{__name__} is online!')
 		
-	@commands.command(alias='av')
-	async def avatar(self, ctx, member: discord.Member=None):
-		if member==None:
-			embedAvatarSelf = discord.Embed(title={message.author}, color=random.randint(-1, 37))
-			embedAvatarSelf.set_image(message.author.avatar)
-			await ctx.reply(embed=embedAvatarSelf)
-		else:
-			embedAvatarOther = discord.Embed(title={member}, color=random.randint(-1, 37))
-			embedAvatarOther.set_image(member.avatar)
-			await ctx.reply(embed=embedAvatarOther)
+    @commands.command(alias='av')
+    async def avatar(self, ctx, member: discord.Member=None):
+        if member==None:
+            embedAvatarSelf = discord.Embed(title={message.author}, color=random.randint(-1, 37))
+            embedAvatarSelf.set_image(message.author.avatar)
+            await ctx.reply(embed=embedAvatarSelf)
+        else:
+            embedAvatarOther = discord.Embed(title={member}, color=random.randint(-1, 37))
+            embedAvatarOther.set_image(member.avatar)
+            await ctx.reply(embed=embedAvatarOther)
 		
 async def setup(bot):
    await bot.add_cog(userProfileCommands(bot))
