70286e7cb9a4248bed645c021766e3c9c6e154af
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2024-08-12 23:03:05 +0530

    Add files via upload

--- pp.py
+++ pp.py
@@ -53,5 +53,5 @@ logger.addHandler(handler)
 async def main():
     async with bot:
         await load()
-        await bot.start(token, log_handler=None)
-asyncio.run(main())
+        await bot.start(token)
+asyncio.run(main(log_handler=None))
