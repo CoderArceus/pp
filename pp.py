a413e2e39297093854d924c7c483da2264df062f
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2024-08-12 23:01:44 +0530

    Add files via upload

--- pp.py
+++ pp.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
-import traceback
+import logging
+import logging.handlers
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -34,14 +35,23 @@ async def reload(ctx):
             print(f'Reloaded {filename} Successfully.')
 
 #LOGGING
-@bot.event
-async def on_error(event, *args, **kwargs):
-    message = args[0] #Gets the message object
-    logging.warning(traceback.format_exc()) #logs the error
-    
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 async def main():
     async with bot:
         await load()
-        await bot.start(token)
+        await bot.start(token, log_handler=None)
 asyncio.run(main())
