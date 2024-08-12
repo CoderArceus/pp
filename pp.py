535087d5589facb8c81d9ce1b694911217a29a23
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2024-08-12 23:10:48 +0530

    Add files via upload

--- pp.py
+++ pp.py
@@ -2,8 +2,6 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
-import logging
-import logging.handlers
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -34,21 +32,6 @@ async def reload(ctx):
             await ctx.send(f'Reloaded {filename} Successfully.')
             print(f'Reloaded {filename} Successfully.')
 
-#LOGGING
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-logging.getLogger('discord.http').setLevel(logging.INFO)
-
-handler = logging.handlers.RotatingFileHandler(
-    filename='discord.log',
-    encoding='utf-8',
-    maxBytes=32 * 1024 * 1024,  # 32 MiB
-    backupCount=5,  # Rotate through 5 files
-)
-dt_fmt = '%Y-%m-%d %H:%M:%S'
-formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 async def main():
     async with bot:
