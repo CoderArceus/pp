5002c65bca3046e460499a7b8d04fae9af2a497d
CoderArceus <75372901+CoderArceus@users.noreply.github.com>
2024-08-14 01:09:15 +0530

    Add files via upload

--- cogs/chatCommands.py
+++ cogs/chatCommands.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import random
+from discord import Spotify
 
 class ChatCommands(commands.Cog):
     def __init__(self, bot):
@@ -12,15 +13,17 @@ class ChatCommands(commands.Cog):
         
 #chat commands start here ->
 
-    @commands.command()
+    @commands.command(aliases=['hi'])
     async def hello(self, ctx):
         await ctx.send('hello!!')
+        
     @commands.command()
     async def roast(self, ctx, member: discord.Member=None):
     	if member==None:
     	    await ctx.reply("you wanna grill yourself lil bro?")
     	else:
         	await ctx.send(f'when girls call {member.mention} smooth, they probably mean their brain')
+            
     @commands.command()
     async def howlong(self, ctx, member: discord.Member=None):
         ppsize=random.randint(-3, 11)
@@ -28,6 +31,17 @@ class ChatCommands(commands.Cog):
             await ctx.reply(f"{ctx.message.author.mention}'s dingus is {ppsize} inches... *oh my*..")
         else:
             await ctx.reply(f"{member.mention}'s dingus is {ppsize} inches... *oh my*..")
-
+            
+    @commands.command()
+    async def spotify(self, ctx, *, user: discord.Member=None):
+    
+        user = user or ctx.author
+        if user.activities:
+            for activity in user.activities:
+                if isinstance(activity, Spotify):
+                    await ctx.reply(f"`{user}` is listening to `{activity.title}` by `{activity.artist}` → [Song Link]({activity.track_url})")
+        else:
+            await ctx.reply(f"`{user}` aint listening no shit")
+        
 async def setup(bot):
    await bot.add_cog(ChatCommands(bot))
--- cogs/userProfileCommands.py
+++ cogs/userProfileCommands.py
@@ -12,33 +12,27 @@ class userProfileCommands(commands.Cog):
 		
     @commands.command(aliases=['av'])
     async def avatar(self, ctx, *, user: discord.Member=None):
-        if not user:
-            member = ctx.author
-        else:
-            member = user
-        
-        userAvatar=member.avatar.url
-        embedAvatar = discord.Embed(title=member, color=discord.Color.random())
+    
+        user = user or ctx.author
+        userAvatar=user.display_avatar.url
+        embedAvatar = discord.Embed(title=user, color=discord.Color.random())
         embedAvatar.set_image(url=userAvatar)
         
         await ctx.reply(embed=embedAvatar)
 
     
-    @commands.command(name="banner", aliases=['b'])
+    @commands.command()
     async def banner(self, ctx, *, user: discord.Member=None):
     
-        if not user:
-            member = ctx.author
-        else:
-            member = user
+        user = user or ctx.author
             
-        fetchedMember = await self.bot.fetch_user(member.id)
+        fetchedUser = await self.bot.fetch_user(user.id)
         
-        if fetchedMember.banner==None:
-            await ctx.reply(f'`{fetchedMember}` got no nitro to have a banner, filthy peasant..')
+        if fetchedUser.banner==None:
+            await ctx.reply(f'`{fetchedUser}` got no nitro to have a banner, filthy peasant..')
         else:
-            userBannerUrl = fetchedMember.banner.url
-            embedBanner = discord.Embed(title=fetchedMember, color = discord.Color.random())
+            userBannerUrl = fetchedUser.banner.url
+            embedBanner = discord.Embed(title=fetchedUser, color = discord.Color.random())
             embedBanner.set_image(url=userBannerUrl)
             await ctx.reply(embed=embedBanner)
 		
